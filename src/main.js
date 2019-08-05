87551037d664ae3a0bda13ead0d46f0780250cda
AdrianaRosas <salamipizza8909@gmail.com>
2019-08-05 18:31:06 -0500

    Añadiendo template string de Login

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const content= document.getElementById('content')
+
+//creando las rutas para entrar a los templates
+const routes={
+    '/': homepage,
+    '/index.html': login,
+}
+
+//investigar esta parte:
+
+window.onpopstate=() => {
+    content.innerHTML= routes[window.location.pathname];
+}
+
+content.innerHTML= routes[window.location.pathname];
---
+++ src/templates/login.js
@@ -0,0 +1,25 @@
+//Template of login
+
+let login=  
+
+`<section class="login" id="login">
+<header>
+    <figure><img src="img/fur-logo.png" alt="Logo fur" id="logo-login"></figure>
+</header>
+<p id="slogan">Porque son más que sólo tus mascotas</p>
+<p>Iniciar sesión con </p>
+<div class="redes">
+    <a id="facebook">
+        <img src="img/facebook-logo.png" alt="Facebook"> Facebook
+    </a>
+    <a id="google">
+        <img src="img/google-icon.png" alt="Google"> Google
+    </a>
+</div>
+<p>O</p>
+<input type="text" class="input-login" placeholder="Usuario">
+<input type="password" name="password" id="password" class="input-login" placeholder="Contraseña">
+<a href="" class="register">Regístrate</a>
+<input type="button" value="Ingresar" class="buttons">
+
+</section>`
