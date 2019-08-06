365fb85e90cc239b46cab345c4935232a551b522
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-08-06 15:19:33 -0500

    Trabajando con SPA

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 // Este es el punto de entrada de tu aplicacion
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 import { myFunction } from './lib/index.js';
-
+import login from './templates/login.js';
 myFunction();
 
-const content= document.getElementById('content')
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+
+// Add the Firebase products that you want to use
+
+//Working with DOM
+const content = document.getElementById('content')
 
 //creando las rutas para entrar a los templates
-const routes={
-    '/': homepage,
-    '/index.html': login,
+const routes = {
+    'src/login': login,
 }
 
 //investigar esta parte:
 
-window.onpopstate=() => {
-    content.innerHTML= routes[window.location.pathname];
+window.onpopstate = () => {
+    content.innerHTML = routes[window.location.pathname];
 }
 
-content.innerHTML= routes[window.location.pathname];
+content.innerHTML = routes[window.location.pathname];
